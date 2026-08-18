c5da64160f852e57581b2ae845223f4521dee11f
Isatech-cloude <omehisaiahn@gmail.com>
2026-08-18 01:53:33 -0700

    Update website design

--- real.js
+++ real.js
@@ -1,4 +1,17 @@
 const button = document.getElementById("getStartedBtn");
 button.addEventListener("click", function(){
-  alert("Welcome to IsaTech!")
+  document.getElementById("services").scrollIntoView({behavior: "smooth"})
+});
+const contactForm = document.getElementById("contactForm");
+contactForm.addEventListener("submit", function(event){
+  event.preventDefault();
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+  if (name == "" || email == "" || message == ""){
+    alert("please fill in all details.");
+    return;
+  }
+  alert("Thank you, " + name + "! Your message has been received")
+  contactForm.reset();
 });
